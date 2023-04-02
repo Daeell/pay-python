299859809abdd9a2bc2c324e731a9b8e5bc00baa
Kim Dael <daelkdev@gmail.com>
2023-04-02 19:12:16 +0900

    수정, 삭제 구현(테스트 통과)

--- personalexpense/views.py
+++ personalexpense/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import PersonalExpenseSerializer
+from .models import PersonalExpense
 
 class ExpenseCreateView(APIView):
     permission_classes = [IsAuthenticated]
@@ -15,48 +16,30 @@ class ExpenseCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ExpenseUpdateDeleteView(APIView):
+    permission_classes = [IsAuthenticated]
 
-# class ExpenseUpdateDeleteView(APIView):
-#     permission_classes = [IsAuthenticated]
+    def get_object(self, pk, user):
+        try:
+            return PersonalExpense.objects.get(pk=pk, user=user)
+        except PersonalExpense.DoesNotExist:
+            return None
 
-#     def get_object(self, pk, user):
-#         try:
-#             return PersonalExpense.objects.get(pk=pk, user=user)
-#         except PersonalExpense.DoesNotExist:
-#             return None
+    def put(self, request, pk):
+        expense = self.get_object(pk, request.user)
+        if expense is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-# class ExpenseUpdateDeleteView(APIView):
-#     permission_classes = [IsAuthenticated]
+        serializer = PersonalExpenseSerializer(expense, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#     def get_object(self, pk):
-#         try:
-#             return PersonalExpense.objects.get(pk=pk, user=self.request.user)
-#         except PersonalExpense.DoesNotExist:
-#             return None
-        
-#     def put(self, request, pk):
-#         expense = self.get_object(pk)
-#         if expense is None:
-#             return Response(status=status.HTTP_404_NOT_FOUND)
-        
-#         serializer = PersonalExpenseSerializer(expense, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        expense = self.get_object(pk, request.user)
+        if expense is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-#     def delete(self, request, pk):
-#         expense = self.get_object(pk)
-#         if expense is None:
-#             return Response(status=status.HTTP_404_NOT_FOUND)
-
-#         expense.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# class ExpenseListView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def get(self, request):
-#         expenses = PersonalExpense.objects.filter(user=request.user)
-#         serializer = PersonalExpenseSerializer(expenses, many=True)
-#         return Response(serializer.data)
+        expense.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
